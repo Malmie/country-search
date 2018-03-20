171fcf8f0f38fd7a9cc8201c63fd9dd869971309
Malmie <miechurska.m88@gmail.com>
2018-03-20 16:46:20 +0100

    code improvement

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
 var url = 'https://restcountries.eu/rest/v2/region/';
 var countriesList = $('#countries');
 
-
 $('#search').click(searchCountries);
 
 function searchCountries() {
-  	regionName = $('#region-name').val();
-  	if(!regionName.length) regionName = 'No data';
-  	$.ajax({
-  		url: url + regionName,
-  		method: 'GET',
-  		success: showCountriesList
-  	});
+	regionName = $('#region-name').val();
+	if(!regionName.length) regionName = 'No data';
+	$.ajax({
+		url: url + regionName,
+		method: 'GET',
+		success: showCountriesList
+	});
 }
 
 function showCountriesList(resp) {
   countriesList.empty();
   resp.forEach(function(item){
       $('<li>').text(item.name).appendTo(countriesList);
-});
+  });
 }
