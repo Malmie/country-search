50813213ccefe1162fde1fba91346dc86525f5e8
Malmie <miechurska.m88@gmail.com>
2018-03-21 11:47:11 +0100

    code improvement

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
-var url = 'https://restcountries.eu/rest/v2/region/';
-var countriesList = $('#countries');
+$(document).ready(function(){
+	var url = 'https://restcountries.eu/rest/v2/region/';
+	var countriesList = $('#countries');
 
-$('#search').click(searchCountries);
+	$('#search').click(searchCountries);
 
-function searchCountries() {
-	regionName = $('#region-name').val();
-	if(!regionName.length) regionName = 'No data';
-	$.ajax({
-		url: url + regionName,
-		method: 'GET',
-		success: showCountriesList
-	});
-}
+	function searchCountries() {
+		regionName = $('#region-name').val();
+		if(!regionName.length) regionName = 'No data';
+		$.ajax({
+			url: url + regionName,
+			method: 'GET',
+			success: showCountriesList
+		});
+	}
 
-function showCountriesList(resp) {
-  countriesList.empty();
-  resp.forEach(function(item){
-      $('<li>').text(item.name).appendTo(countriesList);
-  });
-}
+	function showCountriesList(resp) {
+		countriesList.empty();
+		resp.forEach(function(item){
+			$('<li>').text(item.name).appendTo(countriesList);
+		});
+	}
+});
